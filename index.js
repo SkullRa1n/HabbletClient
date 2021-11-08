0b14109d1d3bf2c2d2dc5f3f7f1e11ec633dfa31
ReversiveDev <caetanorichard41@gmail.com>
2021-11-08 17:36:09 -0300

    -

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 let HabbletClient = require("./HabbletClient");
 
-// não, só se tu quiser enviar alguma modificação pra mim,
-// tipo, tu criou uma parada nova, e achou q vai ficar bom se todos tiverem
-// aí tu faz isso aqui ó
-// salvou o arquivo, o vscode automaticamente vai te dizer os arquivo q tu modificou
-// se vocÊ não quiser enviar a edição de todos eles, vc deleta assim ó
-// e se vc quiser enviar é assim ó
+// E ai nicolau
 
 let client = new HabbletClient(
 	"46a952d3f6532e1c5d0d4d8cf34f456fa80ce80d-36fe32dc8cd73326fdf3b92b9d62794c"
