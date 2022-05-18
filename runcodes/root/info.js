2ba12f1633fa161ed678a7165d115e1069cf63f7
M0rthifxr <chiiobwd@hotmail.com>
2022-05-18 21:06:20 +0100

    ERROR VERSION DO NOT USE

--- runcodes/root/info.js
+++ runcodes/root/info.js
@@ -1,20 +1,20 @@
 let { HabbletClient, Room } = require("habbletclient");
 let path = require('path')
-const SendMessage = require(`../../functions/sendMessage.js`)
+const msg = require(`${path.join(__dirname, `../../functions/sendMessage.js`)}`)
 
 module.exports = {
     aliases: [ 'info', 'sobre'], // Aliases do comando
-    run: async (chatMessage, client, args, answer, SendMessage) => {
+    run: async (chatMessage, client, args, answer) => {
         let user = Room.getUnitByRoomIndex(chatMessage.roomIndex)
-        console.log(SendMessage)
-          await SendMessage(`Bem vindo ${user.name}, à tela de informações`, client);
-          await SendMessage(`Sou um selfbot e não um bot!`, 1, client);
-          await SendMessage(`Bot é um robô sem vida`, 2, client);
-          await SendMessage(`Selfbot é um player com script/cheat!`, 3, client);
-          await SendMessage(`Não kkk, Não estou aqui para floodar`, 4, client);
-          await SendMessage(`Fui editado pela equipe SkullRa1n for u fun`, 5, client);
-          await SendMessage(`Nick do criador: CoreDuo`, 6, client);
-          await SendMessage(`Nick deste bot melhorado: M0rthifxr | Frost.Miller`, 7, client);
-          await SendMessage(`Não fui criado para floodar ou atrapalhar!`, 8, client);
+        console.log(msg)
+          await msg.send(`Bem vindo ${user.name}, à tela de informações`, 0, client);
+          await msg.send(`Sou um selfbot e não um bot!`, 1, client);
+          await msg.send(`Bot é um robô sem vida`, 2, client);
+          await msg.send(`Selfbot é um player com script/cheat!`, 3, client);
+          await msg.send(`Não kkk, Não estou aqui para floodar`, 4, client);
+          await msg.send(`Fui editado pela equipe SkullRa1n for u fun`, 5, client);
+          await msg.send(`Nick do criador: CoreDuo`, 6, client);
+          await msg.send(`Nick deste bot melhorado: M0rthifxr | Frost.Miller`, 7, client);
+          await msg.send(`Não fui criado para floodar ou atrapalhar!`, 8, client);
     }
 }
